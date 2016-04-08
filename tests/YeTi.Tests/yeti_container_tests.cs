2b98d58697690d54a333f348e066b807ace36d80
Jakub Skoczeń <jakub.skoczen@hotmail.com>
2016-04-08 11:33:09 +0200

    implemented basic operations in YeTicontainer

---
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace YeTi.Tests
+{
+    [TestFixture]
+    public class yeti_container_tests
+    {
+        [Test]
+        public void resolves_registered_components()
+        {
+            //arrange
+            var container = new YeTiContainer();
+            container.Register<ITestInterface, TestImplementation>();
+            //act
+            var resolved_object = container.Resolve<ITestInterface>();
+            //arrange
+            resolved_object.ShouldBeOfType<TestImplementation>();
+
+        }
+
+        public interface ITestInterface
+        {
+            
+        }
+
+        public class TestImplementation : ITestInterface
+        {
+            
+        }
+    }
+}
