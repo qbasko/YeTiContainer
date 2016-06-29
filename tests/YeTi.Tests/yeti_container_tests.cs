0caa2ca2ad618de593453a7d216efa74691ac6d8
Jakub Skoczeń <jakub.skoczen@gft.com>
2016-06-29 14:04:19 +0200

    refactored tests

--- tests/YeTi.Tests/yeti_container_tests.cs
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -6,37 +6,51 @@ namespace YeTi.Tests
 {   
     public class yeti_container_tests
     {
+        
+        ITestInterface act()
+        {
+           return _container.Resolve<ITestInterface>();
+        }
+
         [Fact]
         public void resolves_registered_components()
         {
-            var container = new YeTiContainer();
-            container.Register<ITestInterface, TestImplementation>();
-            var resolved_object = container.Resolve<ITestInterface>();
+            _container.Register<ITestInterface, TestImplementation>();
+
+            var resolved_object = act();
+
             resolved_object.ShouldBeOfType<TestImplementation>();
         }
 
         [Fact]
         public void resolves_components_with_ctor_with_dependency()
         {
-            var container = new YeTiContainer();
-            container.Register<ITestInterface, TestImplementationWithDependency>();
-            container.Register<Dependency, Dependency>();
-            var resolved_object = container.Resolve<ITestInterface>();
+            _container.Register<ITestInterface, TestImplementationWithDependency>();
+            _container.Register<Dependency, Dependency>();
+
+            var resolved_object = act();
+
             resolved_object.ShouldBeOfType<TestImplementationWithDependency>();
         }
 
         [Fact]
         public void throws_when_component_has_multiple_ctors()
         {
-            var container = new YeTiContainer();
-            container.Register<ITestInterface, TestImplementationWithMultipleCtors>();
-            container.Register<Dependency, Dependency>();
+            _container.Register<ITestInterface, TestImplementationWithMultipleCtors>();
+            _container.Register<Dependency, Dependency>();
 
-            var exc = Record.Exception(() => container.Resolve<ITestInterface>());
+            var exc = Record.Exception(() => act());
+
             exc.ShouldNotBe(null);
             exc.ShouldBeOfType<ComponentHasMultipleCtorsException>();            
         }
 
+        readonly YeTiContainer _container;
+
+        public yeti_container_tests()
+        {
+            _container = new YeTiContainer();
+        }
 
         public interface ITestInterface
         {
